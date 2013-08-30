3d9a416ab616c9b8604c52ec24c519c253f5c694
majorflight02@gmail.com <majorflight02@gmail.com>
2013-08-30 05:03:35 -0400

    added new beer poj class, and updated db classes and some of the fragments to reflect the changes

---
+++ workspace/pairme/src/com/echolabstech/db/Beer.java
@@ -0,0 +1,17 @@
+package com.echolabstech.db;
+
+public class Beer
+{
+	public long mId;
+	public String mName;
+	public String mType;
+	public String mHead;
+	public String mAroma;
+	public String mAttack;
+	public String mPrimary;
+	public String mSecondary;
+	public String mTertiary;
+	public String mFinal;
+	public String mAfterTaste;
+	public String mBody;
+}//Beer
---
+++ workspace/pairme/src/com/echolabstech/search/SearchBeerEntriesAdapter.java
@@ -0,0 +1,39 @@
+package com.echolabstech.search;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class SearchBeerEntriesAdapter extends BaseAdapter 
+{
+	public static final String TAG = "SearchBeerEntriesAdapter-";
+	public static final boolean DEBUG = true;
+	
+	@Override
+	public int getCount() 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}//getCount
+
+	@Override
+	public Object getItem(int position) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}//getItem
+
+	@Override
+	public long getItemId(int position) 
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}//getItemId
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}//getView
+}//SearchBeerEntriesAdapter
